5b76f7351feb674d4c7cca34ec0ed4e3b3bb9a69
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-26 18:31:29 -0300

    Update nome.py

--- data/nome.py
+++ data/nome.py
@@ -6,7 +6,7 @@ def consultar():
 		nome = ui.dialog1()
 		r =requests.get(url='https://consulta-nome1.p.rapidapi.com/apis/astrahvhdeus/Consultas%20Privadas/HTML/nome.php', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36','x-rapidapi-key': '0d66cf70c4msh8e71af69887c685p1a9b2fjsn8fc892e8b730','x-rapidapi-host': 'consulta-nome1.p.rapidapi.com'}, params={'consulta': nome}).text
 		if 'A consulta Esta Funcionando Normalmente' in r:
-			msg='A consulta está funcionando normalmente, porém, o Telefone inserido não foi encontrado.'
+			msg='A consulta está funcionando normalmente, porém, o Nome inserido não foi encontrado.'
 		else:
 			msg=r.replace('<p>', '').replace('<br>', '\n')
 		op=int(ui.dialog2(msg))
