a1a58df438f692a17e98ac01ac0b08dd67d940bd
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-26 18:31:58 -0300

    Update rg.py

--- data/rg.py
+++ data/rg.py
@@ -6,7 +6,7 @@ def consultar():
 		rg = ui.dialog1()
 		r = requests.get(url='https://consulta-cpf2.p.rapidapi.com/apis/astrahvhdeus/Consultas%20Privadas/HTML/cpf.php', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36','x-rapidapi-key': '0d66cf70c4msh8e71af69887c685p1a9b2fjsn8fc892e8b730','x-rapidapi-host': 'consulta-rg.p.rapidapi.com'}, params={'consulta': rg}).text
 		if 'A consulta Esta Funcionando Normalmente' in r:
-			msg='A consulta está funcionando normalmente, porém, o Telefone inserido não foi encontrado.'
+			msg='A consulta está funcionando normalmente, porém, o RG inserido não foi encontrado.'
 		else:
 			msg=r.replace('<p>', '').replace('<br>', '\n').replace('CPF', '\nCPF')
 		op=int(ui.dialog2(msg))
