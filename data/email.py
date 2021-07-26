b5f3d3851cc143cfe6ec68818d9a57e2e72d1536
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-26 18:31:07 -0300

    Update email.py

--- data/email.py
+++ data/email.py
@@ -6,7 +6,7 @@ def consultar():
 		email = ui.dialog1()
 		r=requests.get(url='https://consulta-e-mail.p.rapidapi.com/apis/astrahvhdeus/Consultas%20Privadas/HTML/email.php', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36','x-rapidapi-key':'0d66cf70c4msh8e71af69887c685p1a9b2fjsn8fc892e8b730','x-rapidapi-host': 'consulta-e-mail.p.rapidapi.com'}, params={'consulta': email}).text
 		if 'A consulta Esta Funcionando Normalmente' in r:
-			msg='A consulta está funcionando normalmente, porém, o Telefone inserido não foi encontrado.'
+			msg='A consulta está funcionando normalmente, porém, o Email inserido não foi encontrado.'
 		else:
 			msg=r.replace('<p>', '').replace('<br>', '\n').replace('CPF', '\nCPF')
 		op=int(ui.dialog2(msg))
