a7a87612103ea382e301057e2974cc16bbc1a265
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-26 18:40:56 -0300

    Update cnpj.py

--- data/cnpj.py
+++ data/cnpj.py
@@ -5,7 +5,7 @@ def consultar():
 	while (Sair==False):
 		cnpj = ui.dialog1()
 		r=requests.get(url=f'https://consulta-cnpj-gratis.p.rapidapi.com/companies/{cnpj}', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36','x-rapidapi-key':'574b13cadbmsh76d5927b45dc8cep17d937jsn0eb56f2d5a5c','x-rapidapi-host': 'consulta-cnpj-gratis.p.rapidapi.com'},params={'consulta': cnpj})
-		if 'A consulta Esta Funcionando Normalmente' in r:
+		if 'error' in r:
 			msg='A consulta está funcionando normalmente, porém, o CNPJ inserido não foi encontrado.'
 		else:
 			msg=f'''
