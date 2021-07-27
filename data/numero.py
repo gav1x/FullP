b1c5aa69cc46ade36a4302a5408313ffee5e7694
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-27 12:53:05 -0300

    Update numero.py

--- data/numero.py
+++ data/numero.py
@@ -5,7 +5,7 @@ def consultar():
 	while (Sair==False):
 		numero = int(ui.dialog1())
 		r = requests.get(url='https://consulta-numero.p.rapidapi.com/apis/astrahvhdeus/Consultas%20Privadas/HTML/numero.php', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36','x-rapidapi-key':'0d66cf70c4msh8e71af69887c685p1a9b2fjsn8fc892e8b730','x-rapidapi-host': 'consulta-numero.p.rapidapi.com'}, params={'consulta': numero}).text
-		if 'A consulta Esta Funcionando Normalmente' in r:
+		if 'A Consulta Esta Funcionando Normalmente' in r:
 			msg='A consulta está funcionando normalmente, porém, o Telefone inserido não foi encontrado.'
 		else:
 			msg=r.replace('<p>', '').replace('<br>', '\n').replace('DDD:', 'DDD: ').replace('NOME:', 'Nome completo: ').replace('FONE:', 'Telefone: ').replace('INST:', 'Data de Compra: ').replace('PESSOA:', 'Pessoa: ').replace('CPF:', 'CPF: ').replace('PESSOA:', 'Pessoa: ')
