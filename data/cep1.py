0626670bd89d6a44213da7269ef00131155a2523
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-27 15:30:03 -0300

    Update cep1.py

--- data/cep1.py
+++ data/cep1.py
@@ -1,8 +1,8 @@
 import requests
 from data import ui
-Sair=False
-while(Sair==False):
-	def consultar():
+def consultar():
+	Sair=False
+	while(Sair==False):
 		cep = int(ui.input_dialog1())
 		r = requests.get(url='https://consulta-cep1.p.rapidapi.com/apis/astrahvhdeus/Consultas%20Privadas/HTML/cep.php', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36','x-rapidapi-key': '0d66cf70c4msh8e71af69887c685p1a9b2fjsn8fc892e8b730','x-rapidapi-host': 'consulta-cep1.p.rapidapi.com'}, params={'consulta': cep})
 		if len(cep)!=8:
