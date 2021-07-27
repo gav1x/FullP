988d52603d9e1b686cd1bf27f84cd5216ca7df41
Kiny-Kiny <70079144+Kiny-Kiny@users.noreply.github.com>
2021-07-27 13:38:54 -0300

    Create cep2.py

---
+++ data/cep2.py
@@ -0,0 +1,33 @@
+import requests
+from data import ui
+# Api - https://github.com/p0isonBR/ConsultaCPF
+Sair=False
+while(Sair==False):
+	def consultar():
+		cep = int(ui.input_dialog1())
+		r = requests.get(url=f'https://viacep.com.br/ws/{cep}/json/')
+		if len(cep)!=8:
+			msg='CEP inválido'
+		try:
+			msg='''
+CEP - {r["cep"]}
+LOGRADOURO - {r['logradouro']}
+COMPLEMENTO - {r['complemento']}
+BAIRRO - {r['bairro']}
+LOCALIDADE - {r['localidade']}
+UF - {r['uf']}
+IBGE - {r['ibge']}
+GIA - {r['gia']}
+DDD - {r['ddd']}
+SIAFI - {r['siafi']}
+'''
+		except:
+			msg='CEP não encontrado.'
+		op=int(ui.input_dialog2(msg))
+		if op==1:
+			pass
+		elif op==2:
+			Sair=True
+		else:
+			ui.error()
+        
