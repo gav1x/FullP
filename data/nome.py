a75b90c2b5f068d546d6c3566167af516790903f
gav1x <86944760+gav1x@users.noreply.github.com>
2021-07-29 18:35:48 -0300

    Update nome.py

--- data/nome.py
+++ data/nome.py
@@ -4,7 +4,7 @@ def consultar():
 	Sair=False
 	while (Sair==False):
 		nome = ui.dialog1()
-		r =requests.get(url='https://consulta-nome1.p.rapidapi.com/apis/astrahvhdeus/Consultas%20Privadas/HTML/nome.php', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36','x-rapidapi-key': '9353b1d886msh73e7a0771d6600dp14ec4cjsn883fd8b4cf13','x-rapidapi-host': 'consulta-nome1.p.rapidapi.com'}, params={'consulta': nome}).text
+		r =requests.get(url='https://consulta-nome1.p.rapidapi.com/apis/astrahvhdeus/Consultas%20Privadas/HTML/nome.php', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36','x-rapidapi-key': 'e01238c690msh74f20bdc84d5dcfp122562jsnc9921fa7c4c1','x-rapidapi-host': 'consulta-nome1.p.rapidapi.com'}, params={'consulta': nome}).text
 		if 'A consulta Esta Funcionando Normalmente' in r:
 			msg='A Consulta está funcionando normalmente, porém, o Nome inserido não foi encontrado.'
 		else:
