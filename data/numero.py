70814edaad37f48ffe88c79116362ca2300b121c
gav1x <86944760+gav1x@users.noreply.github.com>
2021-07-29 18:36:03 -0300

    Update numero.py

--- data/numero.py
+++ data/numero.py
@@ -4,7 +4,7 @@ def consultar():
 	Sair=False
 	while (Sair==False):
 		numero = int(ui.dialog1())
-		r = requests.get(url='https://consulta-numero.p.rapidapi.com/apis/astrahvhdeus/Consultas%20Privadas/HTML/numero.php', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36','x-rapidapi-key':'9353b1d886msh73e7a0771d6600dp14ec4cjsn883fd8b4cf13','x-rapidapi-host': 'consulta-numero.p.rapidapi.com'}, params={'consulta': numero}).text
+		r = requests.get(url='https://consulta-numero.p.rapidapi.com/apis/astrahvhdeus/Consultas%20Privadas/HTML/numero.php', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36','x-rapidapi-key':'e01238c690msh74f20bdc84d5dcfp122562jsnc9921fa7c4c1','x-rapidapi-host': 'consulta-numero.p.rapidapi.com'}, params={'consulta': numero}).text
 		if 'A Consulta Esta Funcionando Normalmente' in r:
 			msg='A consulta está funcionando normalmente, porém, o Telefone inserido não foi encontrado.'
 		else:
